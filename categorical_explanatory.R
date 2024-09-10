7e9637f835837c54a9d5b03138dbfe06ae62c128
Yaniv Brandvain <ybrandvain@gmail.com>
2024-09-10 11:47:05 -0500

    Update categorical_explanatory.R
    added boxplot

--- categorical_explanatory.R
+++ categorical_explanatory.R
@@ -4,7 +4,7 @@ library(dplyr)
 library(ggrepel)
 library(patchwork)
 
-# Make figure a
+# making a jitterplot
 a <-ggplot(filter(penguins, !is.na(sex)), aes(x = species, y = flipper_length_mm,color = species))+
   geom_jitter(size = 3, alpha = .4, show.legend = FALSE,height = 0, width = .3)+
   theme(axis.text = element_text(),
@@ -17,8 +17,21 @@ a <-ggplot(filter(penguins, !is.na(sex)), aes(x = species, y = flipper_length_mm
         plot.background = element_rect(fill= "black",color = "black"))+
   labs(title = "A) geom_jitter()")
 
-# Make figure b
+# making a boxplot
 b <- ggplot(filter(penguins, !is.na(sex)), 
+       aes(x =  species, y = flipper_length_mm,fill = species))+
+  geom_boxplot(show.legend = FALSE)+
+  theme(axis.title.x= element_text(color = "white",face = "bold"),
+        axis.text.x = element_text(color = "white",face = "bold",angle = 20,vjust = 0.65),
+        axis.title.y= element_text(color = "white",face = "bold"),
+        axis.text.y = element_text(color = "white",face = "bold"),
+        plot.title = element_text(color = "white",face = "bold"),
+        axis.ticks = element_line(color = "white"),
+        plot.background = element_rect(fill= "black",color = "black"))+
+  labs(title = "B) geom_boxplot()")
+
+# making a histogram
+c <- ggplot(filter(penguins, !is.na(sex)), 
        aes(x =  flipper_length_mm,fill = species))+
   geom_histogram(show.legend = FALSE)+
   theme(axis.title.x= element_text(color = "white",face = "bold"),
@@ -31,10 +44,10 @@ b <- ggplot(filter(penguins, !is.na(sex)),
   geom_label_repel(data = . %>% group_by(species) %>% summarise(flipper_length_mm = median(flipper_length_mm )),
                    aes(y = 33, label = species), show.legend = FALSE)+
   labs(y = "count")+
-  labs(title = "B) geom_histogram()")
+  labs(title = "C) geom_histogram()")
 
-# Make figure c
-c <- ggplot(filter(penguins, !is.na(sex)), 
+# making a density plot
+d <- ggplot(filter(penguins, !is.na(sex)), 
        aes(x =  flipper_length_mm,fill = species))+
   geom_label_repel(data = . %>% group_by(species) %>% summarise(flipper_length_mm = median(flipper_length_mm )),
                    aes(y = .07, label = species), show.legend = FALSE)+
@@ -47,7 +60,7 @@ c <- ggplot(filter(penguins, !is.na(sex)),
         plot.title = element_text(color = "white",face = "bold"),
         plot.background = element_rect(fill= "black",color = "black"))+
   labs(y = "density")+
-  labs(title = "C) geom_density()")
+  labs(title = "D) geom_density()")
 
-# Put them together with syntax from the patchwork package
-a/(b+c)
+# Putting the plots together (this requires the patchwork package)
+(a+b)/(c+d)
